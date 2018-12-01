0768fa4d4881891ada100010f88c7c000c64b6f6
BrianT <BrianT@192.168.1.15>
2018-12-01 05:30:32 +0100

    Merge origin/JavaEric into JavaEric
    Conflicts:
    	SGP1JAVA/src/sgp1java/SGP1JAVA.java

--- SGP1JAVA/src/sgp1java/SGP1JAVA.java
+++ SGP1JAVA/src/sgp1java/SGP1JAVA.java
@@ -24,27 +24,25 @@ public class SGP1JAVA {
     /**
      * @param args the command line arguments
      */
+
+    public static void Separator(List<Integer> tab,List<Integer> tabS,List<Integer> tabT)
+    {
+        int size = tab.size()/2+tab.size()%2;
+        tabS.addAll(tab.subList(0,size));
+        tabT.addAll(tab.subList(size,tab.size()));
+    }
     public static void main(String[] args){
 
         try {
             List<Integer> tab = new <Integer>ArrayList();
             tab.addAll(Arrays.asList(3,2,5,1,9,4,7,6,8,0,8));
-            int size = tab.size()/2+tab.size()%2;
-            List<Integer> S =  new ArrayList<Integer>(tab.subList(0,size));
-            List<Integer> T = new ArrayList<Integer>(tab.subList(size,tab.size()));
+            List<Integer> S = new ArrayList();
+            List<Integer> T= new ArrayList();
+            Separator(tab,S,T);
             System.out.println(tab.toString());
-            System.out.println(S.toString());
-            System.out.println(T.toString());
-            int minIndex = S.indexOf(Collections.min(S));
-            int maxIndex = T.indexOf(Collections.max(T));
-            
-            /*System.out.println(S.get(minIndex)+" "+minIndex);
-            System.out.println(T.get(maxIndex)+" "+maxIndex);
-            S.remove(minIndex);
-            System.out.println(S.toString());*/
-            
-            int envoieS;
-            int envoieT;
+            System.out.println("S : "+S.toString());
+            System.out.println("T : "+T.toString());
+
             /*while(true)
             {
             int maxS=S.indexOf(Collections.max(S));
@@ -66,24 +64,41 @@ public class SGP1JAVA {
             System.out.println(S.toString());
             System.out.println(T.toString());   */
             
-            ST o = new ST();
-            PipedInputStream res=new PipedInputStream();
-            PipedOutputStream val=new PipedOutputStream();
-            //o.initPipe(val);
-            res.connect(o.IP);
-            val.connect(o.IF);
-            val.write(10);
-            o.start();
+            ST PS = new ST('S');
+            ST PT = new ST('T');
+            PipedInputStream resS=new PipedInputStream();
+            PipedOutputStream valS=new PipedOutputStream();
+            PipedInputStream resT=new PipedInputStream();
+            PipedOutputStream valT=new PipedOutputStream();
+            
+            resS.connect(PS.PPO);
+            valS.connect(PS.PFI);
+            
+            resT.connect(PT.PPO);
+            valT.connect(PT.PFI);
+            
+            PS.ecriture.connect(PT.lecture);
+            PT.ecriture.connect(PS.lecture);
+
+            
+
+            ST.Write(S,valS);
+            ST.Write(T,valT);
+            PS.start();
+            PT.start();
             try {
-                o.join();
+
+                PS.join();
+                PT.join();
             } catch (InterruptedException ex) {
                 Logger.getLogger(SGP1JAVA.class.getName()).log(Level.SEVERE, null, ex);
             }
-            int r =0;
-            while((r=res.read())!=-1)
-            {
-                System.out.println(r);
-            }
+            int r;
+            List<Integer> tmp = new ArrayList<Integer>();
+            ST.Read(tmp,resS);
+            tmp.clear();
+            ST.Read(tmp,resT);
+
         } catch (IOException ex) {
             Logger.getLogger(SGP1JAVA.class.getName()).log(Level.SEVERE, null, ex);
         }
